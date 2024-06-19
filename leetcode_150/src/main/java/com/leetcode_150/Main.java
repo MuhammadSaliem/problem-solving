b5db96f1e2ba0df88c06a9dcfb05a385e77563f4
Saleem <mohamed.saleem@datagearbi.com>
2024-06-19 17:33:50 +0300

    Duplicated integer, is anagram and two sum

---
+++ leetcode_150/src/main/java/com/leetcode_150/Main.java
@@ -0,0 +1,7 @@
+package com.leetcode_150;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ leetcode_150/src/main/java/com/leetcode_150/arrays_and_hashing/easy/two_sum/TwoSum.java
@@ -0,0 +1,31 @@
+package com.leetcode_150.arrays_and_hashing.easy.two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] {
+                        map.get(diff),
+                        i
+                };
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 4, 5, 6 };
+        int[] result = new TwoSum().twoSum(nums, 7);
+
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+    }
+}
